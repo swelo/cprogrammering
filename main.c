2171f0d3ea47adf86e01d1dc3b04466000f479fd
lundin <pontus.lundin@orio.com>
2019-11-18 14:50:45 +0100

    first

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+//Define the struct
+struct weekdays {
+    uint8_t number;
+    char* name;
+};
+
+
+int main()
+{
+	//Declare and initialize a struct to represent week.
+
+
+    //Use the array and print the first and last day
+
+
+    //Create a pointer to the struct array
+  
+	//Use the array pointer and print the first and last day
+
+    return 0;
+}
